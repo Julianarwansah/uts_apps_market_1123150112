50ee0ec13cd864d4693290c1ad1bcca157816254
Julian Arwansah <julianarwansahh@gmail.com>
2026-04-15 16:55:08 +0700

    menambahkan method untuk kirim ulang email verifikasi dan ngecek status verifikasi di AuthProvider

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -122,6 +122,20 @@ class AuthProvider extends ChangeNotifier {
     }
   }
 
+  Future<void> resendVerificationEmail() async {
+    await _firebaseUser?.sendEmailVerification();
+  }
+
+  Future<bool> checkEmailVerified() async {
+    await _firebaseUser?.reload();
+    _firebaseUser = _auth.currentUser;
+
+    if (_firebaseUser?.emailVerified ?? false) {
+      return await _verifyTokenToBackend();
+    }
+    return false;
+  }
+
   Future<bool> loginWithGoogle() async {
     _setLoading();
     try {
