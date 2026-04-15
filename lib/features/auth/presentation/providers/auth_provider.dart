eca64718afd8a7f6c7c69810cddc608663723da3
Julian Arwansah <julianarwansahh@gmail.com>
2026-04-15 16:30:03 +0700

    membuat AuthProvider buat agar mengatur state login pake Firebase,  Google Sign-In

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,3 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 enum AuthStatus {
   initial,
   loading,
@@ -5,4 +9,20 @@ enum AuthStatus {
   unauthenticated,
   emailNotVerified,
   error,
+}
+
+class AuthProvider extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  AuthStatus _status = AuthStatus.initial;
+  User?      _firebaseUser;
+  String?    _backendToken;
+  String?    _errorMessage;
+
+  AuthStatus get status       => _status;
+  User?      get firebaseUser => _firebaseUser;
+  String?    get backendToken => _backendToken;
+  String?    get errorMessage => _errorMessage;
+  bool       get isLoading    => _status == AuthStatus.loading;
 }
