8d2e7ed09706d7fb1fd6f4d5504b9292c00c276a
Julian Arwansah <julianarwansahh@gmail.com>
2026-04-17 08:43:29 +0700

    menambahkan VerifyEmailPage dengan polling verifikasi dan memperbarui AppRouter dengan route dashboard

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,5 +2,6 @@ class AppRouter {
   static const String login       = '/login';
   static const String register    = '/register';
   static const String verifyEmail = '/verify-email';
+  static const String dashboard   = '/dashboard';
   static const String home        = '/home';
 }
