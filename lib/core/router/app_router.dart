7d3c7e32cb90299c5f8b867d66c157f99ec5866a
Julian Arwansah <julianarwansahh@gmail.com>
2026-04-17 08:40:03 +0700

    menambahkan halaman registrasi dan mendefinisikan konstanta routing aplikasi

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,6 @@
+class AppRouter {
+  static const String login       = '/login';
+  static const String register    = '/register';
+  static const String verifyEmail = '/verify-email';
+  static const String home        = '/home';
+}
